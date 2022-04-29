5fec529766b328b00ea9b11c105dccd62d6fa588
mklz <calimiga70@gmail.com>
2022-04-29 02:42:29 +0100

    part 2

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ require("dotenv").config()
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -16,6 +17,12 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi"){
         message.reply("Hello <:peepoRose:968783999566762004>")
     }
+})
+
+const welcomeChannelId = "969178503780122664"
+
+client.on("guildMemberAdd", async (member) => {
+    member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to Mykecord! <:xqcL:968787723433422848>`)
 })
 
 client.login(process.env.TOKEN)
